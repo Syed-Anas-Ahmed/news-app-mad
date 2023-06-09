d6aa0d16465d873d0197a211066fda681af72866
Syed Anas Ahmed <muhananas@gmail.com>
2023-06-10 00:36:27 +0500

    Account.js Screen and App.js
    This is the Account section in the bottom tabs that shows user details from previous logging in through LoginScreen.js and RegisterScreen.js
    
    Also changing App.js to create Navigation throughs different screens, bottom-tabs are also included.
    
    Remaining work: HomeScreen.js & SearchScreen.js and their respective components

--- newsAppMad/App.js
+++ newsAppMad/App.js
@@ -1,20 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo, FontAwesome } from '@expo/vector-icons';
+import HomeScreen from './screens/HomeScreen';
+import SearchScreen from './screens/SearchScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import Account from './Screens/Account';
+
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Home" component={HomeScreen} />
+  </Stack.Navigator>
+);
+
+const SearchStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Search" component={SearchScreen} />
+  </Stack.Navigator>
+);
+
+const AccountStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Account" component={Account} />
+  </Stack.Navigator>
+);
+
+const TabNavigator = () => (
+  <Tab.Navigator
+    screenOptions={{
+      tabBarActiveTintColor: 'black',
+    }}
+  >
+    <Tab.Screen
+      name="HomeStack"
+      component={HomeStack}
+      options={{
+        tabBarIcon: () => <Entypo name="home" size={24} color="black" />,
+        headerShown: false,
+      }}
+    />
+    <Tab.Screen
+      name="SearchStack"
+      component={SearchStack}
+      options={{
+        tabBarIcon: () => <FontAwesome name="search" size={24} color="black" />,
+        headerShown: false,
+      }}
+    />
+
+<Tab.Screen
+      name="Account"
+      component={AccountStack}
+      options={{
+        tabBarIcon: () => <FontAwesome name="user" size={24} color="black" />,
+        headerShown: false,
+        
+      }}
+    />
+
+
+  </Tab.Navigator>
+);
 
 export default function App() {
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={TabNavigator}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
